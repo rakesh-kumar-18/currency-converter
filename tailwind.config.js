163c3a55804db1496401532b724de41eab382137
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2024-05-01 07:50:54 +0530

    change styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'main-bg': "url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
+        'main-bg': "url('https://wallpaperaccess.com/full/7268968.jpg')"
       }
     },
   },
